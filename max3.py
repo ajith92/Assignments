5acef0ae89ff5a4436ce98df003808a3ae3a48e2
ajith92 <endeavour92@gmail.com>
2013-06-14 10:41:46 +0530

    Cmd_line_arguments

--- max3.py
+++ max3.py
@@ -1,10 +1,17 @@
+import sys
 list1=[]
-print "Enter 3 numbers : "
-
-for x in range(0,3):
-	list1.append(int(raw_input()))
-k=0
-for i in range(1,3):
-	if(list1[i]>list1[k]):
-		k=i
-print "largest of 3 is : ",list1[k]
+if len(sys.argv)==4:
+	k=1
+	for i in range(2,4):
+		if(int(sys.argv[i])>int(sys.argv[k])):
+			k=i
+	print "largest of 3 is : ",sys.argv[k]
+else:	
+	print "Enter 3 numbers : "
+	for x in range(0,3):
+		list1.append(int(raw_input()))
+	k=0
+	for i in range(1,3):
+		if(list1[i]>list1[k]):
+			k=i
+	print "largest of 3 is : ",list1[k]
