40e23d10fcf4bbd9fb1a876fbaa43706b26d38d3
Ajith <endeavour92@gmail.com>
2013-06-14 09:25:58 +0530

    first commit

---
+++ max3.py
@@ -0,0 +1,16 @@
+list1=[]
+print "Enter 3 numbers : "
+#k=0
+#for i in range(1,3):
+#	print i
+#	if(list1[i]>list1[k]):
+#		k=i 
+#print list1[k]
+
+for x in range(0,3):
+	list1.append(int(raw_input()))
+k=0
+for i in range(1,3):
+	if(list1[i]>list1[k]):
+		k=i
+print "largest of 3 is : ",list1[k]
